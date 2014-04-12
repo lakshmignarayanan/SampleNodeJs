ddb4352d3c29801b46eb518858d5d14e6179b67e
Lakshmi Narayanan <lakshmignarayanan@outlook.com>
2014-04-13 05:03:31 +0530

    added bootstrap fw

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
+// include required header
 var app = require('http').createServer(handler)
   , io = require('socket.io').listen(app)
   , fs = require('fs')
 var port = Number(process.env.PORT || 5000);
+
+// specify the port number to listen
 app.listen(port);
 
 function handler (req, res) {
-  fs.readFile(__dirname + '/socket.html',
+  fs.readFile(__dirname + '/calc.html',
   function (err, data) {
     if (err) {
       res.writeHead(500);
-      return res.end('Error loading index.html');
+      return res.end('Error loading calc.html');
     }
 
     res.writeHead(200);
@@ -17,14 +20,14 @@ function handler (req, res) {
   });
 }
 
+// create a socket connection
 io.sockets.on('connection', function (socket) {
   socket.emit('news', { hello: 'world' });
   socket.on("result",function(result1){
     console.log("got result "+result1);
     console.log("Sending message "+result1["sum"]+" to all");
+
+    // message all the open clients
     io.sockets.emit('result_display',result1);
   });
-  // socket.on('my other event', function (data) {
-  //   console.log(data);
-  // });
 });
